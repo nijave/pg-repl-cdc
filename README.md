b354c9e8615370443f95a3724ae094c944725069
Nick Venenga <nick@venenga.com>
2024-06-04 16:39:43 -0400

    Move manifests, add README

---
+++ README.md
@@ -0,0 +1,3 @@
+# pg-repl-cdc
+
+Deploys Postgres, minio, pgbench (to generate data), and the Python CDC capture program on Kubernetes.
--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 docker build -t registry.apps.nickv.me/python-cdc-capture .
 docker push registry.apps.nickv.me/python-cdc-capture
-kubectl replace -f job.python-cdc-capture.yaml --force
+(cd manifests && kubectl replace -f job.python-cdc-capture.yaml --force)
